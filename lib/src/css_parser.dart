a2b7c05aa58c07b925c175597dcff3107d97c96e
Riccardo Calligaro <riccardocalligaro@gmail.com>
2020-07-21 16:13:35 +0200

    add rgb support

--- lib/src/css_parser.dart
+++ lib/src/css_parser.dart
@@ -4,39 +4,16 @@ import 'package:csslib/visitor.dart' as css;
 import 'package:csslib/parser.dart' as cssparser;
 import 'package:flutter_html/style.dart';
 
-Map<String, Style> cssToStyles(css.StyleSheet sheet) {
-  sheet.topLevels.forEach((treeNode) {
-    if (treeNode is css.RuleSet) {
-      print(
-          treeNode.selectorGroup.selectors.first.simpleSelectorSequences.first.simpleSelector.name);
-    }
-  });
-}
-
 Style declarationsToStyle(Map<String, List<css.Expression>> declarations) {
   Style style = new Style();
   declarations.forEach((property, value) {
     switch (property) {
       case 'background-color':
-        style.backgroundColor = ExpressionMapping.expressionToColor(value.first);
+        style.backgroundColor =
+            ExpressionMapping.expressionToColor(value.first);
         break;
       case 'color':
         style.color = ExpressionMapping.expressionToColor(value.first);
-        break;
-      case 'direction':
-        style.direction = ExpressionMapping.expressionToDirection(value.first);
-        break;
-      case 'display':
-        style.display = ExpressionMapping.expressionToDisplay(value.first);
-        break;
-      case 'font-family':
-        style.fontFamily = ExpressionMapping.expressionToFontFamily(value.first);
-        break;
-      case 'font-feature-settings':
-        style.fontFeatureSettings = ExpressionMapping.expressionToFontFeatureSettings(value);
-        break;
-      case 'text-shadow':
-        style.textShadow = ExpressionMapping.expressionToTextShadow(value);
         break;
       case 'text-align':
         style.textAlign = ExpressionMapping.expressionToTextAlign(value.first);
@@ -79,12 +56,16 @@ class DeclarationVisitor extends css.Visitor {
 
 //Mapping functions
 class ExpressionMapping {
-
   static Color expressionToColor(css.Expression value) {
     if (value is css.HexColorTerm) {
       return stringToColor(value.text);
+    } else if (value is css.FunctionTerm) {
+      if (value.text == 'rgba') {
+        return rgbOrRgbaToColor(value.span.text);
+      } else if (value.text == 'rgb') {
+        return rgbOrRgbaToColor(value.span.text);
+      }
     }
-    //TODO(Sub6Resources): Support function-term values (rgba()/rgb())
     return null;
   }
 
@@ -102,9 +83,35 @@ class ExpressionMapping {
     }
   }
 
+  static Color rgbOrRgbaToColor(String text) {
+    final rgbaText = text.replaceAll(')', '').replaceAll(' ', '');
+    try {
+      final rgbaValues =
+          rgbaText.split(',').map((value) => double.parse(value)).toList();
+      if (rgbaValues.length == 4) {
+        return Color.fromRGBO(
+          rgbaValues[0].toInt(),
+          rgbaValues[1].toInt(),
+          rgbaValues[2].toInt(),
+          rgbaValues[3],
+        );
+      } else if (rgbaValues.length == 3) {
+        return Color.fromRGBO(
+          rgbaValues[0].toInt(),
+          rgbaValues[1].toInt(),
+          rgbaValues[2].toInt(),
+          1.0,
+        );
+      }
+      return null;
+    } catch (e) {
+      return null;
+    }
+  }
+
   static TextAlign expressionToTextAlign(css.Expression value) {
     if (value is css.LiteralTerm) {
-      switch(value.text) {
+      switch (value.text) {
         case "center":
           return TextAlign.center;
         case "left":
@@ -121,48 +128,4 @@ class ExpressionMapping {
     }
     return TextAlign.start;
   }
-
-  static TextDirection expressionToDirection(css.Expression value) {
-    if (value is css.LiteralTerm) {
-      switch(value.text) {
-        case "ltr":
-          return TextDirection.ltr;
-        case "rtl":
-          return TextDirection.rtl;
-      }
-    }
-    return TextDirection.ltr;
-  }
-
-  static List<FontFeature> expressionToFontFeatureSettings(List<css.Expression> value) {
-    //TODO
-    return [];
-  }
-
-  static List<Shadow> expressionToTextShadow(List<css.Expression> value) {
-    //TODO
-    return [];
-  }
-
-  static Display expressionToDisplay(css.Expression value) {
-    if (value is css.LiteralTerm) {
-      switch(value.text) {
-        case 'block':
-          return Display.BLOCK;
-        case 'inline-block':
-          return Display.INLINE_BLOCK;
-        case 'inline':
-          return Display.INLINE;
-        case 'list-item':
-          return Display.LIST_ITEM;
-      }
-    }
-  }
-
-  static String expressionToFontFamily(css.Expression value) {
-    if(value is css.LiteralTerm)
-      return value.text;
-  }
 }
-
-
